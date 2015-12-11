8f59f665b9af068bfbd34913e7d7d9c30bf7ac4f
Brennan Cheung <git@brennancheung.com>
2015-12-10 22:27:06 -0800

    Setting up scaffold.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+    app:   `${__dirname}/app`,
+    build: `${__dirname}/build`
+};
+
+var common = {
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test:    /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: 'Kanban'})
+    ]
+};
+
+var dev = {
+    devtool: 'eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        hot:       true,
+        inline:    true,
+        progress:  true,
+        stats:     'errors-only',
+        host:      process.env.HOST,
+        port:      process.env.PORT || 4000
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
+
+module.exports = (TARGET == 'start' || !TARGET) ? merge(common, dev) : common;
